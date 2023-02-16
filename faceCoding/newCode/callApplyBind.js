cdc559206af9e45e13db70e1e5f71fe795224372
unknown <1685821150@qq.com>
2023-02-16 22:47:09 +0800

    call, apply, bind

--- faceCoding/newCode/callApplyBind.js
+++ faceCoding/newCode/callApplyBind.js
@@ -32,20 +32,3 @@ Function.prototype.MyBind = function (context, ...args) {
     return result
   }
 }
-
-// demo
-const obj1 = {
-  name: "张三",
-  sayName: function () {
-    console.log(`我是${this.name}`);
-  },
-};
-
-const obj2 = {
-  sayName: function (age, edu) {
-    console.log(`我是${this.name}, 今年${age}, ${edu}学`);
-  },
-};
-// obj1.sayName();
-obj2.sayName.MyBind(obj1)(22, "大1学");
-// obj2.sayName;
