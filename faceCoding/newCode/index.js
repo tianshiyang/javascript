e5ee426cc83f894f95c4b6857fe7b4a0129fcb85
unknown <1685821150@qq.com>
2023-03-10 22:47:45 +0800

    feat: commit

--- faceCoding/newCode/index.js
+++ faceCoding/newCode/index.js
@@ -1,51 +1,12 @@
-Function.prototype.MyCall = function(context, ...args) {
-  if (typeof this !== "function") {
-    return
-  }
-  context = context || window
-  context.fn = this
-  let result = context.fn(...args)
-  delete context.fn
-  return result
+function http(method, url) {
+    let xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.onreadystatechange = function () {
+        if (this.readyState !== 4) {
+            return
+        }
+        if (this.status == 300)
+    }
+    xhr.send()
 }
-
-Function.prototype.MyApply = function (context, args) {
-  if (typeof this !== "function") {
-    return
-  }
-  context = context || window
-  context.fn = this
-  let result = context.fn(...args)
-  delete context.fn
-  return result
-}
-
-Function.prototype.MyBind = function (context, ...args) {
-  if (typeof this !== "function") {
-    return
-  }
-  context = context || window
-  context.fn = this
-  return function(...args2) {
-    let result = context.fn(...args, ...args2)
-    delete context.fn
-    return result
-  }
-}
-
-// demo
-const obj1 = {
-  name: "张三",
-  sayName: function () {
-    console.log(`我是${this.name}`);
-  },
-};
-
-const obj2 = {
-  sayName: function (age, edu) {
-    console.log(`我是${this.name}, 今年${age}, ${edu}学`);
-  },
-};
-// obj1.sayName();
-obj2.sayName.MyBind(obj1)(22, "大1学");
-// obj2.sayName;
